ccd1000cd8a60101010533c9b8cecf2ce9ee94b4
Jhon Lay Lopez <jhonlaylopez000@gmail.com>
2024-12-13 12:44:39 +0800

    add components

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import AllPosts from "./pages/AllPosts.jsx";
 import AddPost from "./pages/AddPost.jsx";
 import EditPost from "./pages/EditPost.jsx";
 import Post from "./pages/Post.jsx";
-import AuthLayout from "./component/AuthLayout.jsx";
+import Protected from "./component/AuthLayout.jsx";
 
 const router = createBrowserRouter([
   {
@@ -26,49 +26,49 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: (
-          <AuthLayout authentication={false}>
+          <Protected authentication={false}>
             <Login />,
-          </AuthLayout>
+          </Protected>
         ),
       },
       {
         path: "/signup",
         element: (
-          <AuthLayout authentication={false}>
+          <Protected authentication={false}>
             <Signup />,
-          </AuthLayout>
+          </Protected>
         ),
       },
       {
         path: "/all-posts",
         element: (
-          <AuthLayout authentication>
+          <Protected authentication>
             <AllPosts />,
-          </AuthLayout>
+          </Protected>
         ),
       },
       {
         path: "/add-posts",
         element: (
-          <AuthLayout authentication>
+          <Protected authentication>
             <AddPost />,
-          </AuthLayout>
+          </Protected>
         ),
       },
       {
         path: "/edit-post/:slug",
         element: (
-          <AuthLayout authentication>
+          <Protected authentication>
             <EditPost />,
-          </AuthLayout>
+          </Protected>
         ),
       },
       {
         path: "/post/:slug",
         element: (
-          <AuthLayout authentication>
+          <Protected authentication>
             <Post />,
-          </AuthLayout>
+          </Protected>
         ),
       },
     ],
